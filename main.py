65098ddd3f91c14993bdd1c0689d99d8e3f96905
Samuel Šušoliak <samuel.susoliak@gmail.com>
2020-09-10 17:42:54 +0200

    progress

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ log_df = pd.read_csv("statuslog1.csv")
 
 supportProjectIds = log_df['supportprojectstatuslog2supportproject'].unique()
 
+missingLogs = []
+
 for supportProjectId in supportProjectIds:
     projectStatusLogs = log_df.loc[log_df['supportprojectstatuslog2supportproject'] == supportProjectId]
-    print(projectStatusLogs)
-
-
+    if 100 not in projectStatusLogs['previousstatus'].tolist():
+        if 0 not in projectStatusLogs['previousstatus'].tolist():
+            firstRow = projectStatusLogs.iloc[0].copy()
+            firstRow['previousstatus'] = 100
+            firstRow['currentstatus'] = 1
+            missingLogs.append(firstRow)
 
